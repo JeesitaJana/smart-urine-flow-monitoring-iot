8ba626ec5e4f92f223267359e7b90d955362c313
Jeesita Jana <jeesitaj@gmail.com>
2026-03-13 19:16:22 +0530

    updated deployement details

--- urine_project/settings.py
+++ urine_project/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$pevo&79u(**px(smt=c$7r_b-8)$k6$$)v#!_o$dl_1ia4+td'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,6 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["uroflow.onrender.com"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
